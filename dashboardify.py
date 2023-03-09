7c63080473cb0b280449e602f98398fe7be1c1a7
packetlifter <carlo.agngaray25@gmail.com>
2023-03-09 10:16:04 +0800

    add logging file

--- dashboardify.py
+++ dashboardify.py
@@ -9,8 +9,8 @@ import sys
 import logging
 import urllib3
 urllib3.disable_warnings()
-
-logging.basicConfig(filename='dashboard.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(message)s', )
+logging.basicConfig(filename='dashboard.log', filemode='w',format='%(asctime)s - %(message)s',level=logging.INFO)
 
 def main(config_file, init):
     
